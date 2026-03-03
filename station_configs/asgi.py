7bf9b36f2db3aa61acd5cdb7d9a439e39beb7658
Andrii Yarotskiy <andriiyarotskiy@gmail,com>
2026-03-03 10:24:22 +0000

    Project formatting

--- station_configs/asgi.py
+++ station_configs/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'station_configs.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "station_configs.settings")
 
 application = get_asgi_application()
